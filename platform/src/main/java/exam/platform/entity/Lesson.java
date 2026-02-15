c54c0ab570001e10392ead3ea5def57202dd6760
Andrey Sergeev <SergeevAndreyAleksandrovich@yandex.ru>
2026-02-15 15:53:12 +0300

    Made Lesson

--- platform/src/main/java/exam/platform/entity/Lesson.java
+++ platform/src/main/java/exam/platform/entity/Lesson.java
@@ -3,11 +3,25 @@ package exam.platform.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="lessons")
 @Data
 public class Lesson {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 150)
+    private String title;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="module_id", nullable = false)
+    private Module module;
+
+    @OneToMany(mappedBy = "lesson", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Assignment> assignemnts = new ArrayList<>();
 
 }
