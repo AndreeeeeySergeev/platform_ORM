5b2c3795bf63ec40dfa592837b0d76357c837bf1
Andrey Sergeev <SergeevAndreyAleksandrovich@yandex.ru>
2026-02-16 00:10:13 +0300

    Made AnswerOption

--- platform/src/main/java/exam/platform/entity/Question.java
+++ platform/src/main/java/exam/platform/entity/Question.java
@@ -1,4 +1,32 @@
 package exam.platform.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "questions")
+@Data
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String text;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private QuestionType type;
+
+    public enum QuestionType {
+        SINGLE_CHOICE, MULTIPLE_CHOICE
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="quiz_id", nullable = false)
+    private Quiz quiz;
+
+    @OneToMany(mappedBy = "question", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<AnswerOption> options = new ArrayList<>();
 }
--- platform/src/main/java/exam/platform/entity/Quiz.java
+++ platform/src/main/java/exam/platform/entity/Quiz.java
@@ -1,10 +1,29 @@
 package exam.platform.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="quizzes")
+@Data
 public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length=100, nullable = false)
+    private String title;
+
+    @Column(name="time_limit")
+    private Integer timeLimit;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="module_id", nullable = false)
     private Module module;
+
+    @OneToMany(mappedBy = "quiz", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Question> questions = new ArrayList<>();
 }
